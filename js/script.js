00ebd4c7417fc4c874de92a8c366ab12b34a37c5
romero007andryxa <92734953+romero007andryxa@users.noreply.github.com>
2023-04-10 21:30:04 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () =>{
+    menu.classList.add('active');
+});
+closeElem.addEventListener('click', () =>{
+    menu.classList.remove('active');
+});
+
+const percents = document.querySelectorAll('.skills__skillbars-percentage'),
+      lines = document.querySelectorAll('.skills__skillbars-line span');
+
+percents.forEach( (item, i) =>{
+    lines[i].style.width = item.innerHTML;
+})
